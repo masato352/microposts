326a10d534ddc50e37c3c3446a457e4869b513bc
masato352 <masato_tateishi@yahoo.co.jp>
2016-07-11 13:37:51 +0000

    add edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,18 +20,4 @@ class SessionsController < ApplicationController
   end
 
 
-  def edit
-  end
-
-  def update
-    if @name.update(name_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'プロフィールを編集しました'
-    else
-      # 保存に失敗した場合は編集画面へ戻す
-      render 'edit'
-    end
-  end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+    
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile, :area )
   end  
   
 end
